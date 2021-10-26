316e17e9f3c9f147cabbc1ac7d8f179e1763506c
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-10-27 01:50:06 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+### Somewhat usefull Kubernetes and Codefresh related scripts and aliases
+
+    codefresh.sh included functions:
+
+|||
+|----------:|:-------------|
+| sa_token() | decode pod's sa jwt 
+| ns() | set or display current namespace
+|  get_trigger() | get trigger used for the last or specific build
+|  pipid() | set PIPID variable by running latest 'cf get pip' from history
+| cf_token() | get token of the specific or current context
+| cf_api_key() | set CF_API_KEY var from the cf_token() output |
+| keys() | wrapper around jq(1) displays keys by default
+
+ Useful for quick review of the json files, i.e:
+
+     cat pipeline.json | keys    # only top level keys
+     cat pipeline.json | keys spec   # all the values under .spec
+     cat pipeline.json | keys spec.steps.   # only keys under steps, note the "dot"
+
+* * *
+
+`git_convert_remote.sh` - converts git remote specification from https to ssh  and vice versa and performs inline edits
+
+* * *
+`bash_functions.sh` - includes system helper functions
+|||
+|----------:|:-------------|
+| dir() | searches for the pattern in files in current directory (default)
+Wrapper around find(1).
+
+    Usage: [GREP_OPTS] dir [DIR] [DEPTH] EXPR
+
+Arguments can be used interchangeably, see comments for details.
+
+
+
+
---
+++ bash_functions.sh
@@ -0,0 +1,59 @@
+#### Just a functions for easier life ####
+
+#  
+# Usage: [GREP_OPTS] dir [DIR] [DEPTH] EXPR
+#
+# find(1) wrapper, displays filenames in which pattern was found. Searched is
+# performed only in the current directory unless "max" specified. 
+# 
+# Instead of writing:
+#   find . -maxdepth 1 -type f -exec grep -ln {} -e pattern \; 
+#
+# you just do: 
+#   dir pattern
+#     
+#
+# Note the options to grep(1). It can be changed by passing another values
+# explicitly on the command line by prepending command with grep_opts=
+#
+# DIR, DEPTH AND PATTERN can be passed in any order. Only PATTERN is mandatory. 
+# Special value of "max" for DEPTH will cause search to recurse to the directories.
+
+grep_opts="--color --with-filename --line-number";
+function dir() 
+{ 
+  # Accept asterisk '*' for any depth
+  test -n "$GREP_OPTS" && grep_opts=$GREP_OPTS
+  # Must by delcared as such, otherwise they retain values after a call
+  declare ERROR
+  
+  if [ $# -gt 3 ]; then
+    echo "Excess number of arguments specified" >&2 
+    ((ERROR++)) 
+  elif [  $# -eq 0 ]; then
+    echo "Search pattern must be specified!" >&2;
+    ((ERROR++)) 
+  fi
+  test -z "$ERROR" || { echo "Usage: [grep_opts] dir [DIR] [DEPTH] EXPR" >&2; \
+                        return 1; }
+  expr=${!#}
+  while [ $# -gt 1 ]
+  do
+    if test $1 = 'max'
+    then
+      depth='300' 
+    elif test -e $1; then
+      dir=$1
+    else
+      depth=$1
+    fi
+    shift;
+  done;
+#  echo "dir: $dir"
+#  echo "depth: $depth"
+#  echo "expr: $expr"
+# return 2
+  #echo "find ${dir:-.} -maxdepth ${depth:-1} -type f -exec grep ${grep_opts} {} -e \"$expr\" \;"
+  find ${dir:-.} -maxdepth ${depth:-1} -type f -exec grep ${grep_opts} {} -e "$expr" \;
+}
+
