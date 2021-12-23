d6ef8a298d1bbae60d71c553b41bb0ca7556839c
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-12-23 19:45:29 +0200

    backup

---
+++ backup/cf-get-pip.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# Get yaml from repo under .spec.specTemplates.steps. That will keep output
+# of `cf get pip` updated with real steps defintion when "yaml from repo" is used.
+
+test -n "$1" || \
+  { echo "Wrapper around \`codefresh get pipeline'"; 
+    echo "Run \`codefresh get pipeline --help' for the available options";
+    #echo "In case of questions on script implementation and desired modifications contact Codefresh support."
+    exit 1; 
+  }
+
+
+# CF_API_KEY can be exported before, so config file parsing happens. Useful
+# when ~/.config file is not in the standard location.
+if [ -z "$CF_API_KEY" ]; then
+  # Path to config file, required to retrieve API token
+  cfconfig=~/.cfconfig
+
+  # Must have before we do anything at all..
+  test -e "$cfconfig" || { echo "Config file is missing"; exit 1; }
+
+  # Context
+  ctx=$( cat ~/.cfconfig | yq e '."current-context"' -)
+
+  # Token
+  CF_API_KEY=$( cat ~/.cfconfig | yq e ".contexts.$ctx.token" -  | tr -d \" ) 
+fi
+
+
+### Done with token part
+
+# Keep original format
+format=`echo $@ | sed -n 's/.*-o *\(yaml\|json\).*/\1/p'`
+
+# If format was not specified no steps are dumped, conform with original command
+if test -z "$format" 
+then 
+  codefresh get pip "$@"
+  exit
+fi
+
+# Just a random string, avoiding mktemp(1) because of compatibility issues with MacOS.
+dump_file=.6jzZcSfWCd
+
+# Run the command and store output in json array to fit further processing
+eval $(echo codefresh get pip "$@" | sed 's/'"$format"'/json/') | jq 'if type == "object" then [.] else . end' 2>/dev/null > $dump_file
+
+# Fail if no data was retrieved
+test -s "$dump_file" || { echo "No data was retrieved"; rm $dump_file;  exit 1; }
+
+#Check this file for the dump if needed
+#echo $dump_file
+
+# Iterate through pipeline id's and operate on corresponding blocks of definition within single file
+for id in $( cat $dump_file | yq eval '.[].metadata.id' - )
+do
+  # Get data needed for API call from .spec.specTemplate
+  request_data=$( yq --prettyPrint eval '.[] | select(.metadata.id == "'$id'").spec.specTemplate' $dump_file )
+  
+  # if .spec.specTemplate field was present
+  if grep -qv null <<<"$request_data";
+  then 
+  # initialize variables for the call
+    eval $( echo "$request_data" | sed 's/: */=/;' | sed '/path/ {s,./,,; s,/,%2F,g}' )
+
+    # Get yaml from repository
+    from_repo=$( curl -s -X GET -H "Authorization: $CF_API_KEY" https://g.codefresh.io/api/repos/${repo}/${revision}/${path/}?context=${context} )
+
+    # Get only the steps in json
+    steps=$( echo "$from_repo" | yq e .content - | yq e .steps - -o=json)
+
+    # Write steps under .spec.specTemplate.steps
+    yq -P -i e '( .[] | select(.metadata.id == "'$id'").spec.steps |= '"$steps"') | [.]' $dump_file
+  fi
+done
+
+# When multiline pipelines, put them under items array; conforming to the `cf get pip -o yaml` output
+if [ `yq eval length $dump_file` -gt 1 ]
+then
+  if [ $format = yaml ]
+  then
+    yq -P -i e '. = {"items": .}'  $dump_file 
+  fi
+  # no modification is needed for json
+else
+  # when single pipeline, remove it from the array
+  yq -i e '.[]' $dump_file
+fi
+
+# Print out up-to-dated pipelines
+yq -P e . -o=$format $dump_file 
+
+# Remove temporary files
+#echo $dump_file
+rm $dump_file 
