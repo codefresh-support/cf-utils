652cad308530fb5c945bac6d525a3bfcfbd07115
Laurent <laurent.rochette@codefresh.io>
2022-03-23 11:17:02 -0700

    new version of the runtime uninstaller

---
+++ csdp/csdp-runtime-uninstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Delete CSDP runtime:
+
+# Check parameter numbers
+if [ $# -ne 1 ]; then
+  echo "Incorrect number of arguments"
+  echo "Usage:"
+  echo "cf-runtime-uninstall.sh RUNTIME_NAME"
+  exit 1
+fi
+NAME="$1"
+
+echo "Removing CSDP runtime $NAME"
+cf runtime uninstall --force "$NAME" --silent
+
+echo "Deleting remaining objects"
+# Edit apps to remove finalizers
+for i in $(kubectl get applications -o $NAME)
+do
+  kubectl patch  $i --type json --patch='[ { "op": "remove", "path": "/metadata/finalizers" } ]'
+done
+for OBJ in apps deployments replicaset pods service statefulset
+do
+  kubectl get $OBJ --no-headers -n $NAME| awk '{print $1}' | xargs kubectl  -n $NAME delete $OBJ
+done
+
+echo "Deleting namespace $NAME"
+kubectl delete ns $NAME
