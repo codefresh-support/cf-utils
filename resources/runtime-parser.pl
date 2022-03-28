4daa5408b0bdf96dece86f55d14d867d7cf8f7b3
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2022-03-28 23:17:02 +0300

    resources

--- resources/runtime-parser.pl
+++ resources/runtime-parser.pl
@@ -7,9 +7,9 @@ use YAML::Tiny;
 my $yaml = YAML::Tiny->read('re.yaml');
 my $runtime = $yaml->[0];     # reference to the first document
 print "$runtime\n";
-foreach my $key (keys %$runtime) {
-  print "key: $key\n";
-}
+#foreach my $key (keys %$runtime) {
+#  print "key: $key\n";
+#}
 my $requests = $yaml->[0]->{dockerDaemonScheduler}->{defaultDindResources}->{requests};
 #print $requests->{cpu},"\n";
 #$requests->{cpu} = '1000m';
