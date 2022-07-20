24576bbbc624d146294cde67738631a352469a8e
Oleg Sergiyuk <oleg.sergiyuk@codefresh.io>
2022-07-21 00:03:29 +0300

    13835

---
+++ classic/success_failed_stat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Call to api/workflow endpoint and gets statuses of the builds of specific pipeline
+
+usage() {
+  echo "Usage: `basename $0` [--from=YYYY-MM-DD] [--to=YYYY-MM-DD] PIPELINE_ID"
+}
+
+while [ "1" ]; do
+  ((PAGE++))
+  result=$(codefresh get builds --limit 20 $* --page $PAGE --sc created,status )
+  #echo "$PAGE A"
+  grep -q "no available resources" <<<$result && break
+  builds=$( echo -e "$builds\n" "$result" | sed '/^$/d; /CREATED/d' )
+done
+echo "$builds"
+
